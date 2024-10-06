06b6aaf7ccb1a427fbff29fbf08828778b21b0ee
Victor Celis <victor.celis.c88@gmail.com>
2024-10-06 13:58:54 -0300

    Modelos Casi Finalizados

--- app/models.py
+++ app/models.py
@@ -66,13 +66,38 @@ class Preferencia(db.Model):
     tipo_subsidio = db.Column(db.String(15), nullable=True)
     notificacion = db.Column(db.Boolean, nullable=True)
     busqueda_automatica = db.Column(db.Boolean, nullable=True)
+    distancia = db.Column(db.Float, nullable=True)
     contactado = db.Column(db.Boolean, nullable=False, default=True)
     id_user = db.Column(db.String(10), db.ForeignKey('user.id_user'), nullable=False)
 
     idx_preferencia_id_user = db.Index('idx_preferencia_id_user', id_user)
+    idx_preferencia_distancia = db.Index('idx_preferencia_distancia', distancia)
 
     def __repr__(self):
         return f'<Preferencia {self.id_preferencia}>'
+    
+    def serialize(self):
+        return {
+            'id': self.id_preferencia,
+            'area_total': self.area_total,
+            'pisos': self.pisos,
+            'habitaciones': self.habitaciones,
+            'precio_uf': self.precio_uf,
+            'estaciona': self.estaciona,
+            'bodega': self.bodega,
+            'antiguedad': self.antiguedad,
+            'tipo_vivienda': self.tipo_vivienda,
+            'condicion': self.condicion,
+            'tipo_operacion': self.tipo_operacion,
+            'banos': self.banos,
+            'area_construida': self.area_construida,
+            'tipo_subsidio': self.tipo_subsidio,
+            'notificacion': self.notificacion,
+            'busqueda_automatica': self.busqueda_automatica,
+            'distancia': self.distancia,
+            'contactado': self.contactado,
+            'id_user': self.id_user
+        }
 
     def save(self):
         db.session.add(self)
@@ -85,7 +110,277 @@ class Preferencia(db.Model):
         db.session.delete(self)
         db.session.commit()
 
+class Region(db.Model):
+    __tablename__ = 'region'
+    id_region = db.Column(db.Integer, primary_key=True)
+    nombre_region = db.Column(db.String(100), nullable=False)
 
+    def __repr__(self):
+        return f'<Region {self.nombre_region}>'
+    
+    def serialize(self):
+        return {
+            'id': self.id_region,
+            'nombre': self.nombre_region
+        }
 
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
 
+    def update(self):
+        db.session.commit()
 
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+class Ciudad(db.Model):
+    __tablename__ = 'ciudad'
+    id_ciudad = db.Column(db.Integer, primary_key=True)
+    nom_ciudad = db.Column(db.String(100), nullable=False)
+    id_region = db.Column(db.Integer, db.ForeignKey('region.id_region'), nullable=False)
+
+    def __repr__(self):
+        return f'<Ciudad {self.nom_ciudad}>'
+    
+    def serialize(self):
+        return {
+            'id': self.id_ciudad,
+            'nombre': self.nom_ciudad,
+            'id_region': self.id_region
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+class Comuna(db.Model):
+    __tablename__ = 'comuna'
+    id_comuna = db.Column(db.Integer, primary_key=True)
+    nom_comuna = db.Column(db.String(100), nullable=False)
+    id_ciudad = db.Column(db.Integer, db.ForeignKey('ciudad.id_ciudad'), nullable=False)
+    id_region = db.Column(db.Integer, db.ForeignKey('region.id_region'), nullable=False)
+
+    def __repr__(self):
+        return f'<Comuna {self.nom_comuna}>'
+    
+    def serialize(self):
+        return {
+            'id': self.id_comuna,
+            'nombre': self.nom_comuna,
+            'id_ciudad': self.id_ciudad
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+class Vecindario(db.Model):
+    __tablename__ = 'vecindario'
+    id_vecindario = db.Column(db.Integer, primary_key=True)
+    nom_vecindario = db.Column(db.String(100), nullable=False)
+    id_comuna = db.Column(db.Integer, db.ForeignKey('comuna.id_comuna'), nullable=False)
+    id_ciudad = db.Column(db.Integer, db.ForeignKey('ciudad.id_ciudad'), nullable=False)
+    id_region = db.Column(db.Integer, db.ForeignKey('region.id_region'), nullable=False)
+
+    def __repr__(self):
+        return f'<Vecindario {self.nom_vecindario}>'
+    
+    def serialize(self):
+        return {
+            'id': self.id_vecindario,
+            'nombre': self.nom_vecindario,
+            'id_comuna': self.id_comuna
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+class Vivienda(db.Model):
+    __tablename__ = 'vivienda'
+    id_vivienda = db.Column(db.String(10), primary_key=True)
+    area_total = db.Column(db.Float, nullable=True)
+    pisos = db.Column(db.Integer, nullable=True)
+    habitaciones = db.Column(db.Integer, nullable=True)
+    precio_uf = db.Column(db.Float, nullable=False)
+    estaciona = db.Column(db.Integer, nullable=True)
+    bodega = db.Column(db.Integer, nullable=True)
+    antiguedad = db.Column(db.Integer, nullable=True)
+    tipo_vivienda = db.Column(db.Integer, nullable=False)
+    nombre_propiedad = db.Column(db.String(250), nullable=False)
+    descripcion = db.Column(db.String(500), nullable=False)
+    condicion = db.Column(db.String(10), nullable=True)
+    tipo_operacion = db.Column(db.Boolean, nullable=True)
+    banos = db.Column(db.Integer, nullable=True)
+    area_construida = db.Column(db.Float, nullable=True)
+    latitud = db.Column(db.Float, nullable=False)
+    longitud = db.Column(db.Float, nullable=False)
+    tipo_subsidio = db.Column(db.String(15), nullable=True)
+    fecha_creacion = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now(datetime.timezone.utc))
+    links_contacto = db.Column(JSON, nullable=True)
+    id_vecindario = db.Column(db.Integer, db.ForeignKey('vecindario.id_vecindario'), nullable=False)
+    id_comuna = db.Column(db.Integer, db.ForeignKey('vecindario.id_comuna'), nullable=False)
+    id_ciudad = db.Column(db.Integer, db.ForeignKey('vecindario.id_ciudad'), nullable=False)
+    id_region = db.Column(db.Integer, db.ForeignKey('vecindario.id_region'), nullable=False)
+
+    idx_vivienda_id_vecindario = db.Index('idx_vivienda_id_vecindario', id_vecindario)
+    idx_vivienda_latitud = db.Index('idx_vivienda_latitud', latitud)
+    idx_vivienda_longitud = db.Index('idx_vivienda_longitud', longitud)
+
+    def __repr__(self):
+        return f'<Vivienda {self.nombre_propiedad}>'
+    
+    def serialize(self):
+        return {
+            'id': self.id_vivienda,
+            'area_total': self.area_total,
+            'pisos': self.pisos,
+            'habitaciones': self.habitaciones,
+            'precio_uf': self.precio_uf,
+            'estaciona': self.estaciona,
+            'bodega': self.bodega,
+            'antiguedad': self.antiguedad,
+            'tipo_vivienda': self.tipo_vivienda,
+            'nombre_propiedad': self.nombre_propiedad,
+            'descripcion': self.descripcion,
+            'condicion': self.condicion,
+            'tipo_operacion': self.tipo_operacion,
+            'banos': self.banos,
+            'area_construida': self.area_construida,
+            'latitud': self.latitud,
+            'longitud': self.longitud,
+            'tipo_subsidio': self.tipo_subsidio,
+            'fecha_creacion': self.fecha_creacion.strftime('%d-%m-%Y %H:%M:%S'),
+            'links_contacto': self.links_contacto,
+            'id_vecindario': self.id_vecindario,
+            'id_comuna': self.id_comuna,
+            'id_ciudad': self.id_ciudad,
+            'id_region': self.id_region
+        }
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+class Imagen(db.Model):
+    __tablename__ = 'imagen'
+    id_imagen = db.Column(db.String(10), primary_key=True)
+    url = db.Column(db.String(500), nullable=False)
+    id_vivienda = db.Column(db.String(10), db.ForeignKey('vivienda.id_vivienda'), nullable=False)
+
+    def __repr__(self):
+        return f'<Imagen {self.id_imagen}>'
+    
+    def serialize(self):
+        return {
+            'id': self.id_imagen,
+            'url': self.url,
+            'id_vivienda': self.id_vivienda
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+class Favorito(db.Model):
+    __tablename__ = 'favorito'
+    id_favorito = db.Column(db.String(5), primary_key=True)
+    fecha_guardado = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now(datetime.timezone.utc))
+    id_user = db.Column(db.String(10), db.ForeignKey('user.id_user'), nullable=False)
+    id_vivienda = db.Column(db.String(10), db.ForeignKey('vivienda.id_vivienda'), nullable=False)
+
+    idx_favorito_id_user = db.Index('idx_favorito_id_user', id_user)
+    idx_favorito_id_vivienda = db.Index('idx_favorito_id_vivienda', id_vivienda)
+
+    def __repr__(self):
+        return f'<Favorito {self.id_favorito}>'
+    
+    def serialize(self):
+        return {
+            'id': self.id_favorito,
+            'id_user': self.id_user,
+            'fecha_guardado': self.fecha_guardado.strftime('%d-%m-%Y %H:%M:%S'),
+            'id_vivienda': self.id_vivienda
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+class Match(db.Model):
+    __tablename__ = 'match'
+    id_match = db.Column(db.String(5), primary_key=True)
+    fecha_coincidencia = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now(datetime.timezone.utc))
+    visto = db.Column(db.Boolean, nullable=False, default=False)
+    id_user = db.Column(db.String(10), db.ForeignKey('user.id_user'), nullable=False)
+    id_vivienda = db.Column(db.String(10), db.ForeignKey('vivienda.id_vivienda'), nullable=False)
+
+    idx_favorito_id_user = db.Index('idx_favorito_id_user', id_user)
+    idx_favorito_id_vivienda = db.Index('idx_favorito_id_vivienda', id_vivienda)
+    
+    def __repr__(self):
+        return f'<Match {self.id_match}>'
+    
+    def serialize(self):
+        return {
+            'id': self.id_match,
+            'id_user': self.id_user,
+            'fecha_coincidencia': self.fecha_coincidencia.strftime('%d-%m-%Y %H:%M:%S'),
+            'visto': self.visto,
+            'id_vivienda': self.id_vivienda
+        }
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
