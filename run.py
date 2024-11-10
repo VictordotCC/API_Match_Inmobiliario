c6db6846d8ff05e1f92bbd22064f3507b130701d
Magnus-Lekter <man.vargasc@duocuc.cl>
2024-11-10 00:06:35 -0300

    se agrega archico requirements.txt para subir a Render la API

--- run.py
+++ run.py
@@ -5,4 +5,7 @@ from flask_cors import CORS
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app = create_app()
+    #app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run()
+    
