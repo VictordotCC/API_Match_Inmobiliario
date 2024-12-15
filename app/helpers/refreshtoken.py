521b360f4763befe010ee3946acaa886d871336a
Victor Celis Contreras <67768778+VictordotCC@users.noreply.github.com>
2024-12-15 13:48:37 -0300

    Actualizar refreshtoken.py

--- app/helpers/refreshtoken.py
+++ app/helpers/refreshtoken.py
@@ -5,7 +5,7 @@ import os
 
 def refresh_token():
 
-    path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+    """path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))"""
         
     client_id = os.environ.get('CLIENT_ID')
     client_secret = os.environ.get('CLIENT_SECRET')
